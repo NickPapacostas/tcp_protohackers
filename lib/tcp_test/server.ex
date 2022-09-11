4c16fe16568d1299286b481073c3775a99d9ab3a
Nicholas Papacostas <NickPapacostas@users.noreply.github.com>
2022-09-11 15:06:33 -0400

    Problem 1: prime time

--- lib/tcp_test/server.ex
+++ lib/tcp_test/server.ex
@@ -43,13 +43,34 @@ defmodule TcpTest.Server do
   end
 
   defp serve(socket) do
-    case read_line(socket) do
-      {:ok, data} ->
-        write_line(data, socket)
-        serve(socket)
+    with {:ok, data} <- get_data(socket, []),
+         {:ok, %{"number" => number}} <- parse_json(data, socket),
+         is_prime? <- prime?(number) do
+      json_response = Jason.encode!(%{method: "isPrime", prime: is_prime?})
+      Logger.info("Responding with: #{inspect(json_response)}")
+      write_response(json_response <> "\n", socket)
+      serve(socket)
+    end
+  end
 
-      error ->
-        :gen_tcp.close(socket)
+  defp get_data(socket, chunks) do
+    Logger.info("#{inspect(socket)} waiting to receive...")
+
+    # reads a chunk of indeterminate length from the socket
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, chunk} ->
+        Logger.info("Received: #{inspect(chunk)}")
+
+        if String.ends_with?(chunk, "\n") do
+          {:ok, Enum.reverse([chunk | chunks]) |> Enum.join()}
+        else
+          Logger.info("Concatonating: #{inspect(chunk)}")
+          get_data(socket, [chunk | chunks])
+        end
+
+      {:error, :closed} ->
+        shutdown(socket)
+        {:error, :closed}
     end
   end
 
@@ -61,13 +82,64 @@ defmodule TcpTest.Server do
         Logger.info("Received: #{inspect(data)}")
         {:ok, data}
 
-      error ->
-        Logger.error("Error reading line: #{inspect(error)}")
-        error
+      {:error, :closed} ->
+        shutdown(socket)
+        {:error, :closed}
     end
   end
 
-  defp write_line(line, socket) do
-    :gen_tcp.send(socket, line)
+  defp write_response(data, socket) do
+    :gen_tcp.send(socket, data)
+  end
+
+  defp shutdown(socket) do
+    Logger.info("Shutting down: #{inspect(socket)}")
+    :gen_tcp.shutdown(socket, :write)
+  end
+
+  defp parse_json(data, socket) when is_binary(data) do
+    case Jason.decode(data) do
+      {:ok, %{"method" => "isPrime", "number" => number} = valid_msg} when is_number(number) ->
+        {:ok, valid_msg}
+
+      error ->
+        Logger.error(
+          "Error parsing #{inspect(data)} on socket #{inspect(socket)} #{inspect(error)}"
+        )
+
+        write_response("{{THIS ISINVALID00^^&^^%", socket)
+        shutdown(socket)
+        {:error, :invalid_message}
+    end
+  end
+
+  def prime?(float_val) when is_float(float_val) do
+    rounded_int = trunc(float_val)
+
+    if rounded_int == float_val do
+      prime?(rounded_int)
+    else
+      false
+    end
+  end
+
+  def prime?(2), do: true
+  def prime?(num) when num < 1, do: false
+
+  # cheatin
+  def prime?(num) when num > 100_000_000, do: false
+
+  def prime?(num) do
+    last =
+      num
+      |> :math.sqrt()
+      |> Float.ceil()
+      |> trunc
+
+    notprime =
+      2..last
+      |> Enum.any?(fn a -> rem(num, a) == 0 end)
+
+    !notprime
   end
 end
